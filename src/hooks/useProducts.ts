9a8cd4d8904b09102ce0b11971360c97d2b6ad8d
Sevda <sevda.amini@codexconsulting.com.au>
2025-01-06 16:14:29 +1000

    custom hook and pages added

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,16 @@
+import { Product } from "@/interface/interface";
+import { useEffect, useState } from "react";
+
+const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return products;
+};
